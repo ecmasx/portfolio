b367a810ede59a5d04c6aaf0404357a051dac618
maximmadan <maximmadan3@gmail.com>
2024-10-25 20:21:55 +0300

    fix: starting project

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -4,12 +4,12 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../../public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "../../public", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`server started on http://localhost:${PORT}`);
+  console.log(`Server started on http://localhost:${PORT}`);
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/styles/*{html,js}"],
+  content: ["./public/*.html", "./src/scripts/*.js"],
   theme: {
     extend: {
       padding: {
